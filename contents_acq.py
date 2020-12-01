afe0cdb617ef21b209fa0d2c98e35a34532ca8a2
sunmuffin42 <sunkulp.ananth@gmail.com>
2020-11-30 23:43:47 -0600

    make and python files

---
+++ contents_acq.py
@@ -0,0 +1,10 @@
+import re, sys
+old_string = "".join([i for i in sys.stdin])
+metadata = re.compile(r"\n([rwx\-]{10}.+(20|:)\d{2} |d.*)")
+dirinfo = re.compile(r"\n.+\ntotal \d+")
+lines = re.compile(r"\n{2,}")
+renew = old_string
+for regex, replace in zip([metadata,dirinfo,lines], [r"\n", "", r"\n"]):
+    renew = re.sub(regex, replace, renew)
+
+sys.stdout.write(renew)
