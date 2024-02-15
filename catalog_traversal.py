428ab2068b2f84aa72ff7334a6aa2dafebc3f6be
sunmuffin42 <sunkulp.ananth@gmail.com>
2024-02-14 16:58:19 -0800

    add date to contents and add catalog jump up one folder for search

--- catalog_traversal.py
+++ catalog_traversal.py
@@ -1,17 +1,13 @@
 import os, json
 
-path = "./"
-# os.walk(pathcd)
-
-# for i in os.walk(path):
-#     i # will return tuple(absolute/path/to/pwd, [dirs], [files])
-#         # then the next item will be the dirs[0] as the pwd
-#         # depth first
+path = os.getcwd() + "/.." # if you put the entire "contents" folder into the top level directory you want to search
+# path = "./" # if you just put the makefile and python files directly into the top level directory you want to search
 
 for dirpath, dirnames, filenames in os.walk(path):
     directory_level = dirpath.replace(path, "")
     directory_level = directory_level.count(os.sep)
     indent = " " * 4
     print("{}{}/".format(indent*directory_level, os.path.basename(dirpath)))
-    for f in [filename for filename in filenames if filename[-3:].lower() in ["pdf", "txt", "jvu"]]:
+    # for f in [filename for filename in filenames if filename[-3:].lower() in ["pdf", "txt", "jvu"]]:
+    for f in [filename for filename in filenames if filename[-3:].lower()]:
         print("{}{}".format(indent*(directory_level+1), f))
--- makefile
+++ makefile
@@ -1,9 +1,13 @@
 PYTHON := python
 
+DATE = $(shell date +'%Y%m%d')
+
+# all:
+#     $(PYTHON) catalog_traversal.py > "catalog_$(DATE).out"
 contents: contents_acq.py
 	ls -R -o > contents
-	$(PYTHON) contents_acq.py < contents > contents.out
+	$(PYTHON) contents_acq.py < contents > "contents_$(DATE).out"
 	rm -f contents
 
 catalog: catalog_traversal.py
-	$(PYTHON) catalog_traversal.py > catalog.out
+	$(PYTHON) catalog_traversal.py > "catalog_$(DATE).out"
